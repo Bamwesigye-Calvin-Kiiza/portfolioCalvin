c6cdd68e0c96d8eaa034b3aafce1d0c7cba0bac6
Bamwesigye-Calvin-Kiiza <bamwesigyecalvinkiiza@gmail.com>
2026-02-17 10:53:59 +0300

    Improved static file discovery for production

--- config/settings.py
+++ config/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
 
 # Media files (App uploads)
 MEDIA_URL = "/media/"
